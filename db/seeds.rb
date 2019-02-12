a21db5c8894cea7f62ea141b3ba2896609d6e44d
Claire Fahy <zeebear21@gmail.com>
2019-02-12 17:25:49 -0300

    butt-ugly, but it works. Next up: CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "creating articles…"
+10.times do
+  article = Article.new(
+    title: Faker::Food.unique.dish,
+    content: Faker::Food.unique.description
+    )
+  article.save!
+end
+
+puts "finished creating articles"
